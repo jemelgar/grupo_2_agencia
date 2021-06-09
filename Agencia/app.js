37e7570103e4ccf9293c1bad350772c343233815
Enrique Melgar <j_enriquem@hotmail.com>
2021-06-09 16:23:24 -0500

    Se agregaron las conexiones en el app.js para las vistas html. El servidor local corre en el puerto 3000

---
+++ Agencia/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static("public"));
+app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("views/index.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve("views/login.html"));
+});
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve("views/productCart.html"));
+});
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve("views/productDetail.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve("views/register.html"));
+});
