91321e37f46a5fb858f30283f5f5c84e9206dbf4
Andrea Cazares <andreacazares1412@gmail.com>
2021-06-13 09:15:12 -0400

    login.html

--- Agencia/app.js
+++ Agencia/app.js
@@ -7,9 +7,11 @@ app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("views/index.html"));
 });
+
 app.get("/login", (req, res) => {
   res.sendFile(path.resolve("views/login.html"));
 });
+
 app.get("/productCart", (req, res) => {
   res.sendFile(path.resolve("views/productCart.html"));
 });
