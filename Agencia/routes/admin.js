c8c8f2bcad4d8774dfe2896eb5676c4c8edfeeb5
Enrique Melgar <j_enriquem@hotmail.com>
2021-07-08 17:22:30 -0500

    Se agrega la opción de crear producto y modificar, queda pendiente el CSS

--- Agencia/routes/admin.js
+++ Agencia/routes/admin.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const adminController = require('../controllers/adminController');
+const adminController = require("../controllers/adminController");
 
+router.get("/", adminController.index);
+router.get("/edit/:id", adminController.edit);
+router.put("/edit/:id", adminController.saveEdit);
 
-router.get('/', adminController.edit);
+router.post("/", adminController.addProducto);
+router.get("/new", adminController.newProduct);
 
 module.exports = router;
