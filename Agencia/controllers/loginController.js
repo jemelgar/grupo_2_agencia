73792e8e9ebab2b40e03368c73791bde076116a5
Enrique Melgar <j_enriquem@hotmail.com>
2021-08-15 16:28:26 -0500

    Procesamiento de Cookies funcionando

--- Agencia/controllers/loginController.js
+++ Agencia/controllers/loginController.js
@@ -1,55 +1,58 @@
-const {validationResult }= require('express-validator')
-const User = require('../models/Users');
-const bcryptjs = require('bcryptjs');
+const { validationResult } = require("express-validator");
+const User = require("../models/Users");
+const bcryptjs = require("bcryptjs");
 
 const controladorLogin = {
-	login: (req, res) => {
-		res.render('login');
-	},
-	loginProcess:(req, res) => {
-    let userToLogin = User.findByField('email', req.body.email)
+  login: (req, res) => {
+    res.render("login");
+  },
+  loginProcess: (req, res) => {
+    let userToLogin = User.findByField("email", req.body.email);
 
-		if(userToLogin){
-			let comparePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
-			if(comparePassword)  {
-				delete userToLogin.password;
-				req.session.userLogged = userToLogin
+    if (userToLogin) {
+      let comparePassword = bcryptjs.compareSync(
+        req.body.password,
+        userToLogin.password
+      );
+      if (comparePassword) {
+        delete userToLogin.password;
+        req.session.userLogged = userToLogin;
 
-				if(req.body.remember_user){
-					res.cookie('userEmail', req.body.email, {maxAge: (1000 * 60) * 2})
-				}
-				return res.redirect('/login/profile')
-			}
-			return res.render('login', {
-		     	errors:{
-					email: {
-						msg: 'Las credenciales son inválidas'
-					}
-			   	}
-			})
-		}
-		
-		return res.render('login', {
-			errors:{
-			email: {
-				msg: 'El email no se encuentra registrado'
-			}
-		}
-		}) 
-	},
-	profile: (req, res) => {
-		console.log(req.cookies.userEmail)
-		console.log(req.session.userLogged)
-		return res.render('profile', {
-			user: req.session.userLogged
-		});	
-	},
-	
-	logout: (req, res) => {
-		req.session.destroy();
-		console.log(req.session)
-		return res.redirect('/')
-	}
+        if (req.body.remember_user) {
+          res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 * 4 });
+        }
+        return res.redirect("/login/profile");
+      }
+      return res.render("login", {
+        errors: {
+          email: {
+            msg: "Las credenciales son inválidas",
+          },
+        },
+      });
+    }
 
+    return res.render("login", {
+      errors: {
+        email: {
+          msg: "El email no se encuentra registrado",
+        },
+      },
+    });
+  },
+  profile: (req, res) => {
+    // console.log(req.cookies.userEmail);
+    // console.log(req.session.userLogged);
+    return res.render("profile", {
+      user: req.session.userLogged,
+    });
+  },
+
+  logout: (req, res) => {
+    res.clearCookie("userEmail");
+    req.session.destroy();
+    // console.log(req.session);
+    return res.redirect("/");
+  },
 };
 module.exports = controladorLogin;
