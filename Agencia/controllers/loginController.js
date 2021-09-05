416d2a0f2b693055edadf2d6e0778b54e0cd2acb
Mareli95 <mareli.rubio.izaguirre@gmail.com>
2021-09-05 15:23:30 -0500

    Login validaciones listo

--- Agencia/controllers/loginController.js
+++ Agencia/controllers/loginController.js
@@ -1,62 +1,63 @@
-const { validationResult } = require("express-validator");
-const User = require("../models/Users");
-const bcryptjs = require("bcryptjs");
-
+const { validationResult } = require('express-validator');
+// const User = require('../models/Users');
+const bcryptjs = require('bcryptjs');
+const db = require('../database/models');
 const controladorLogin = {
-  login: (req, res) => {
-    res.render("login");
-  },
-  loginProcess: (req, res) => {
-    let userToLogin = User.findByField("email", req.body.email);
+	login: (req, res) => {
+		res.render('login');
+	},
+	loginProcess: (req, res) => {
+		db.Usuario.findOne({
+			where: {
+				email: req.body.email,
+			},
+		}).then((userToLogin) => {
+			if (userToLogin) {
+				let comparePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
+				if (comparePassword) {
+					delete userToLogin.password;
+					req.session.userLogged = userToLogin;
 
-    if (userToLogin) {
-      let comparePassword = bcryptjs.compareSync(
-        req.body.password,
-        userToLogin.password
-      );
-      if (comparePassword) {
-        delete userToLogin.password;
-        req.session.userLogged = userToLogin;
+					if (req.body.remember_user) {
+						res.cookie('userEmail', req.body.email, { maxAge: 1000 * 60 * 4 });
+					}
+					if (req.session.userLogged.tipoUsuario != 'Administrador') {
+						return res.redirect('/login/profile');
+					} else {
+						return res.redirect('/admin');
+					}
+				}
+				return res.render('login', {
+					errors: {
+						email: {
+							msg: 'Las credenciales son inválidas',
+						},
+					},
+				});
+			}
 
-        if (req.body.remember_user) {
-          res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 * 4 });
-        }
-        if (req.session.userLogged.tipoUsuario != "Administrador") {
-          return res.redirect("/login/profile");
-        } else {
-          return res.redirect("/admin");
-        }
-      }
-      return res.render("login", {
-        errors: {
-          email: {
-            msg: "Las credenciales son inválidas",
-          },
-        },
-      });
-    }
+			return res.render('login', {
+				errors: {
+					email: {
+						msg: 'El email no se encuentra registrado',
+					},
+				},
+			});
+		});
+	},
+	profile: (req, res) => {
+		// console.log(req.cookies.userEmail);
+		// console.log(req.session.userLogged);
+		return res.render('profile', {
+			user: req.session.userLogged,
+		});
+	},
 
-    return res.render("login", {
-      errors: {
-        email: {
-          msg: "El email no se encuentra registrado",
-        },
-      },
-    });
-  },
-  profile: (req, res) => {
-    // console.log(req.cookies.userEmail);
-    // console.log(req.session.userLogged);
-    return res.render("profile", {
-      user: req.session.userLogged,
-    });
-  },
-
-  logout: (req, res) => {
-    res.clearCookie("userEmail");
-    req.session.destroy();
-    // console.log(req.session);
-    return res.redirect("/");
-  },
+	logout: (req, res) => {
+		res.clearCookie('userEmail');
+		req.session.destroy();
+		// console.log(req.session);
+		return res.redirect('/');
+	},
 };
 module.exports = controladorLogin;
--- Agencia/routes/pruebaCRUD.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controllerCrud = require("../controllers/controllerCRUD");
-
-router.post("/", controllerCrud.create);
-
-module.exports = router;
