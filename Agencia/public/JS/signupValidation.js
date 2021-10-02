9db63ba7325e20895289d3e9cc8ee7cb9f92c294
Mareli95 <mareli.rubio.izaguirre@gmail.com>
2021-10-02 10:49:05 -0500

    Añadiendo más validaciones de contraseña en signup

--- Agencia/public/JS/signupValidation.js
+++ Agencia/public/JS/signupValidation.js
@@ -60,6 +60,19 @@ window.onload = function () {
 		} else if (password.value.length < 8) {
 			errores.push('La contraseña debe tener mínimo 8 caracteres');
 			password.classList.add('is-invalid');
+		}
+		// Más validaciones de Contraseña
+		// Contener al menos un dígito
+		else if (!/\d/.test(password.value)) {
+			errores.push('La contraseña debe contener al menos un dígito');
+			password.classList.add('is-invalid');
+			// Contener al menos una minúscula
+		} else if (!/[a-z]/.test(password.value)) {
+			errores.push('La contraseña debe contener al menos una mínuscula');
+			password.classList.add('is-invalid');
+		} else if (!/[A-Z]/.test(password.value)) {
+			errores.push('La contraseña debe contener al menos una mayúscula');
+			password.classList.add('is-invalid');
 		} else {
 			password.classList.add('is-valid');
 			password.classList.remove('is-invalid');
