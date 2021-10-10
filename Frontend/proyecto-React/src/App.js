675912a76662213d7543924a70f33862a71385d3
Mareli95 <mareli.rubio.izaguirre@gmail.com>
2021-10-09 20:36:15 -0500

    Dividiendo carpetas

---
+++ Frontend/proyecto-React/src/App.js
@@ -0,0 +1,15 @@
+import Productos from './components/Productos';
+import Usuarios from './components/Usuarios';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Usuarios/>
+      <Productos/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
