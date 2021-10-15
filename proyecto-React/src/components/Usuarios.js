72c667fd062eaeca38d37bb0f4db0fd8372fadcd
Mareli95 <mareli.rubio.izaguirre@gmail.com>
2021-10-14 23:20:42 -0500

    Listado de productos y usuarios feo

--- proyecto-React/src/components/Usuarios.js
+++ proyecto-React/src/components/Usuarios.js
@@ -1,24 +1,66 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useState } from 'react';
 
 function Usuarios() {
+	const [usuarios, setUsuarios] = useState([]);
+	const usuariosApi = async () => {
+		const url = 'admin/usuariosApi';
+		const success = await fetch(url);
+		const successJson = await success.json();
+		setUsuarios(successJson.data);
+	};
+	useEffect(() => {
+		console.log('Usuarios');
+		usuariosApi();
+	}, []);
 
-const usuariosApi = async() =>{
-    const url = 'admin/usuariosApi';
-    const success = await fetch(url)
-    const successJson = await success.json();
-    console.log(successJson)
-}
-useEffect(()=> {
-    console.log('inicializando!!!!');
-    usuariosApi();
-},[])
+	return (
+		<div>
+			<h2>Usuarios</h2>
+			<ul>
+				{/* {usuarios.lenght === 0 && <h2>Cargando</h2>} */}
+				{usuarios.map((usuario, i) => {
+					return (
+						<li key={i}>
+							<h3>{usuario.first_name + ' ' + usuario.last_name}</h3>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 
-  return (
-   
-    <div >
-     <h2> Soy el componente usuarios!!!! </h2>
-    </div>
-  )
+	// **-----------------------------------------
+	// ! No mover //
+	// const [usuarios, setUsuarios] = useState([]);
+	// useEffect(() => {
+	// 	fetch('admin/usuariosApi')
+	// 		.then((res) => {
+	// 			return res.json();
+	// 		})
+	// 		.then((user) => {
+	// 			setUsuarios(user.data);
+	// 		})
+	// 		.catch((error) => {
+	// 			console.log(error);
+	// 		});
+	// }, []);
+	// return (
+	// 	<div>
+	// 		<h2>Usuarios</h2>
+	// 		<ul>
+	// 			{/* {usuarios.lenght === 0 && <h2>Cargando</h2>} */}
+	// 			{usuarios.map((usuario, i) => {
+	// 				return (
+	// 					<li key={i}>
+	// 						<h3>{usuario.first_name}</h3>
+	// 					</li>
+	// 				);
+	// 			})}
+	// 		</ul>
+	// 	</div>
+	// );
+	// * ------------------------------------------------//
+	// !No mover //
 }
 
 export default Usuarios;
