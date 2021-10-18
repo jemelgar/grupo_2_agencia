5a88b5f48f65a3a808d89fb3f97b6ee7144605ea
Enrique Melgar <j_enriquem@hotmail.com>
2021-10-17 19:07:32 -0500

    se agregó la información del panel de categorías

--- Agencia/database/config/config.js
+++ Agencia/database/config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
-	development: {
-		username: 'Mariana',
-		password: 'Hola123',
-		database: 'agencia',
-		host: '127.0.0.1',
-		dialect: 'mysql',
-		port: 3307,
-	},
-	test: {
-		username: 'root',
-		password: null,
-		database: 'database_test',
-		host: '127.0.0.1',
-		dialect: 'mysql',
-	},
-	production: {
-		username: 'root',
-		password: null,
-		database: 'database_production',
-		host: '127.0.0.1',
-		dialect: 'mysql',
-	},
+  development: {
+    username: "root",
+    password: "toor",
+    database: "agencia2",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    port: 3307,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
 };
