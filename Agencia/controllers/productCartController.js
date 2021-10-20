570c5b19fbe4985a0b6e13a73a6cb6dcf805e646
Enrique Melgar <j_enriquem@hotmail.com>
2021-10-19 20:31:35 -0500

    hotfixes

--- Agencia/controllers/productCartController.js
+++ Agencia/controllers/productCartController.js
@@ -1,25 +1,34 @@
 // const destinodata = require('../database/db-ignored.json');
 const fs = require("fs");
 const path = require("path");
-const toursFilePath = path.join(__dirname, "../database/db-ignored.json");
-const tours = JSON.parse(fs.readFileSync(toursFilePath, "utf-8"));
+// const toursFilePath = path.join(__dirname, "../database/db-ignored.json");
+// const tours = JSON.parse(fs.readFileSync(toursFilePath, "utf-8"));
+const db = require("../database/models");
 
 const controlador = {
   productCar: (req, res) => {
     res.render("carrito");
   },
+
   productCarById: (req, res) => {
     let tourSelected;
     let formInfo = req.body;
-    let productId = req.params.id;
-    for (let tour of tours.destinos) {
-      if (tour.tag == productId) {
-        tourSelected = tour;
-        break;
-      }
-    }
+    // console.log(req.body);
 
-    res.render("carrito", { tourSelected, formInfo });
+    db.Producto.findByPk(req.params.id).then((tourSelected) => {
+      // console.log(tourSelected);
+      res.render("carrito", { tourSelected, formInfo });
+      //return res.json(tours);
+    });
+
+    // for (let tour of tours.destinos) {
+    //   if (tour.tag == productId) {
+    //     tourSelected = tour;
+    //     break;
+    //   }
+    // }
+
+    // res.render("carrito", { tourSelected, formInfo });
   },
 };
 
