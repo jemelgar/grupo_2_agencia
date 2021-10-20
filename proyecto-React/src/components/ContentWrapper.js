cc6c55ffe82d72fb95a131c51bab136effe8c0a6
Enrique Melgar <j_enriquem@hotmail.com>
2021-10-20 12:19:42 -0500

    modificaciones panel

--- proyecto-React/src/components/ContentWrapper.js
+++ proyecto-React/src/components/ContentWrapper.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ContentRow from './ContentRow.js';
-import TopBar from './TopBar.js';
+import React from "react";
+import ContentRow from "./ContentRow.js";
+import TopBar from "./TopBar.js";
 
 function ContentWrapper() {
-	return (
-		<React.Fragment>
-			{/*<!-- Content Wrapper -->*/}
-			<div id="content-wrapper" className="d-flex flex-column">
-				{/*<!-- Main Content -->*/}
-				<div id="content">
-				     <TopBar />
-					<ContentRow />
-				</div>
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      {/*<!-- Content Wrapper -->*/}
+      <div id="content-wrapper" className="d-flex flex-column">
+        {/*<!-- Main Content -->*/}
+        <div id="content">
+          {/* <TopBar /> */}
+          <ContentRow />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 export default ContentWrapper;
--- proyecto-React/src/components/Sidebar.js
+++ proyecto-React/src/components/Sidebar.js
@@ -1,67 +1,65 @@
-import React from 'react';
-import image from '../assets/img/Logo.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+// import image from '../assets/img/Logo.png';
+import { Link } from "react-router-dom";
 function SideBar() {
-	return (
-		<React.Fragment>
-			{/*<!-- Sidebar -->*/}
-			<ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
-				{/*<!-- Sidebar - Brand -->*/}
-				<a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-					<div className="sidebar-brand-icon">
-						<img className="w-100" src= {image} width= "10%" alt="Focus Travel" />
-					</div>
-				</a>
+  return (
+    <React.Fragment>
+      {/*<!-- Sidebar -->*/}
+      <ul
+        className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion"
+        id="accordionSidebar"
+      >
+        {/*<!-- Sidebar - Brand -->*/}
 
-				{/*<!-- Divider -->*/}
-				<hr className="sidebar-divider my-0" />
+        {/*<!-- Divider -->*/}
+        <hr className="sidebar-divider my-0" />
 
-				{/*<!-- Nav Item - Dashboard -->*/}
-				<li className="nav-item active">
-					<Link className="nav-link" to="/">
-						<i className="fas fa-fw fa-tachometer-alt"></i>
-						<span>Dashboard Administrador</span>
-					</Link>
-				</li>
+        {/*<!-- Nav Item - Dashboard -->*/}
+        <li className="nav-item active">
+          <Link className="nav-link" to="/">
+            <i className="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard Administrador</span>
+          </Link>
+        </li>
 
-				{/*<!-- Divider -->*/}
-				<hr className="sidebar-divider" />
+        {/*<!-- Divider -->*/}
+        <hr className="sidebar-divider" />
 
-				{/*<!-- Heading -->*/}
-				<div className="sidebar-heading">Actions</div>
+        {/*<!-- Heading -->*/}
+        <div className="sidebar-heading">Actions</div>
 
-				{/*<!-- Generos -->*/}
-				<li className="nav-item">
-					<Link className="nav-link" to="/usuariosApi">
-						<i className="fas fa-fw fa-users"></i>
+        {/*<!-- Generos -->*/}
+        <li className="nav-item">
+          <Link className="nav-link" to="/usuariosApi">
+            <i className="fas fa-fw fa-users"></i>
 
-						<span>Listado de Usuarios</span>
-					</Link>
-				</li>
+            <span>Listado de Usuarios</span>
+          </Link>
+        </li>
 
-				{/*<!-- Generos -->*/}
-				<li className="nav-item">
-					<Link className="nav-link" to="/productosApi">
-						<i className="fas fa-fw fa-list"></i>
+        {/*<!-- Generos -->*/}
+        <li className="nav-item">
+          <Link className="nav-link" to="/productosApi">
+            <i className="fas fa-fw fa-list"></i>
 
-						<span>Listado de Productos</span>
-					</Link>
-				</li>
+            <span>Listado de Productos</span>
+          </Link>
+        </li>
 
-				{/*<!-- Last Movie -->*/}
-				<li className="nav-item">
-					<Link className="nav-link" to="/categorias">
-						<i className="fas fa-fw fa-list"></i>
+        {/*<!-- Last Movie -->*/}
+        <li className="nav-item">
+          <Link className="nav-link" to="/categorias">
+            <i className="fas fa-fw fa-list"></i>
 
-						<span>Categorias</span>
-					</Link>
-				</li>
+            <span>Categorias</span>
+          </Link>
+        </li>
 
-				{/*<!-- Divider -->*/}
-				<hr className="sidebar-divider d-none d-md-block" />
-			</ul>
-			{/*<!-- End of Sidebar -->*/}
-		</React.Fragment>
-	);
+        {/*<!-- Divider -->*/}
+        <hr className="sidebar-divider d-none d-md-block" />
+      </ul>
+      {/*<!-- End of Sidebar -->*/}
+    </React.Fragment>
+  );
 }
 export default SideBar;
